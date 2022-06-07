355d71eb0f234bbe519b877ddff2a72689af6ef0
Joey <jpbougneit@gmail.com>
2022-06-07 20:16:46 +0200

    First commit

--- src/main/java/com/randomtaskgenerator/RandomTaskGeneratorPlugin.java
+++ src/main/java/com/randomtaskgenerator/RandomTaskGeneratorPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.randomtaskgenerator;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Random Task Generator"
 )
-public class ExamplePlugin extends Plugin
+public class RandomTaskGeneratorPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private RandomTaskGeneratorConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	RandomTaskGeneratorConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(RandomTaskGeneratorConfig.class);
 	}
 }
